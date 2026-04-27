a94372fb10495a9967a78443fe406ae6215b7622
akeesar1 <akeesar1@charlotte.edu>
2026-04-26 20:23:30 -0400

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,28 @@
-import Navbar from "./Navbar";
+import { Link } from "react-router-dom";
 
 export default function Header() {
     return (
         <header>
             <h1>Abhinav's React Demo - 3/23/26</h1>
-            <Navbar />
+            
+            <nav>
+                <Link to="/">Home</Link>{" | "}
+                <Link to="/contract">Contract</Link>{" | "}
+                <Link to="/website-evaluations">Web Evaluations</Link>{" | "}
+                <Link to="/survey">Survey</Link>{" | "}
+                <Link to="/cards">Cards</Link>{" | "}
+                <Link to="/inventory">Inventory</Link>{" | "}
+                <Link to="/instructions">Instructions</Link>{" | "}
+                <Link to="/product">Product</Link>
+            </nav>
+
+            <nav>
+                <Link to="/crappy">Crappy Page</Link>{" | "}
+                <Link to="/hobby">Hobby Page</Link>{" | "}
+                <Link to="/intro-form">Intro Form</Link>{" | "}
+                <Link to="/slideshow">slideshow Page</Link>{" | "}
+            </nav>
+
         </header>
     );
 }
